d1babd297adb649e3ec0b914a8ead39982fcc335
EgorArndt <devegorarndt@gmail.com>
2022-05-19 12:36:05 +0400

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from './components/ui-kit'
+import './sass/main.scss'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button 
+        text='Aproval' 
+        className='palette-success-400 dark-h' 
+      />
+       <Button 
+        text='Aproval' 
+        className='palette-primary-600 light-h' 
+      />
+      <span className='color-primary-600'>
+        U!
+      </span>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/ui-kit/types.ts
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export type DefaultProps = {
+    as?: keyof React.ReactHTML
+    children?: Children<any> 
+    className?: string
+    style?: React.CSSProperties
+    ref?: React.RefObject<HTMLElement>
+}
+export type ResponsiveSize = 's' | 'm' | 'l'
+export type SupportsArray<T> = T | T[]
+export type Component<Props> = React.ComponentClass<Props> | React.FC<Props>
+export type Element<Props> = React.ReactElement<Props>
+export type Children<Props> = SupportsArray<Props>
